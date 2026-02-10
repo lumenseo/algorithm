a628d5a3a12e10dc34834cebf50e80d0e7810e50
lumenseo <leslumiere1@gmail.com>
2026-02-10 14:47:21 +0900

    [Bronze II] Title: 이진수 변환, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/10829. 이진수 변환/이진수 변환.py
+++ 백준/Bronze/10829. 이진수 변환/이진수 변환.py
@@ -1,13 +1,14 @@
+# return 으로 활용하기
+
 # 시작점: N
-# 끝점: 1
-# 누적값(전달되는 값): N//2 
-
+# 끝점: 0
+# 누적값(전달되는값): N//2
 def recur(num):
-    if num==0:
-        return 
-    
-    recur(num//2) # 2로 나눈 몫을 전달  # 일단 끝까지 들어갔다가
-    print(num%2, end='')  # 되돌아오면서 출력하자 # 어떤 걸 할것인지를 줄 위치를 제대로
+    if num == 0:
+        return ""
 
-N=int(input())
-recur(N)
+    return recur(num // 2) + str(num % 2)
+
+
+N = int(input())
+print(recur(N))
