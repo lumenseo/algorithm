1e946135c793e675be4058a9c9f5adfc1bd1421a
lumenseo <leslumiere1@gmail.com>
2026-02-10 14:40:41 +0900

    [Bronze II] Title: 이진수 변환, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/10829. 이진수 변환/이진수 변환.py
+++ 백준/Bronze/10829. 이진수 변환/이진수 변환.py
@@ -1,11 +1,13 @@
-stack=[]
-def binary(num):
-    global stack
+# 시작점: N
+# 끝점: 1
+# 누적값(전달되는 값): N//2 
+
+def recur(num):
     if num==0:
-        return
-    binary(num//2)
-    stack.append(num%2)
+        return 
+    
+    recur(num//2) # 2로 나눈 몫을 전달  # 일단 끝까지 들어갔다가
+    print(num%2, end='')  # 되돌아오면서 출력하자 # 어떤 걸 할것인지를 줄 위치를 제대로
 
 N=int(input())
-binary(N)
-print(''.join(map(str, stack)))
+recur(N)
